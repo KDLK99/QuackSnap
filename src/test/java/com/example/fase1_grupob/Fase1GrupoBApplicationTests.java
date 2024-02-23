1a12df67a00182dfdc42680a5cbd55761eca7e7d
Xerneax8 <mosmalopez@gmail.com>
2024-02-23 12:15:11 +0100

    CreacionProyecto

---
+++ src/test/java/com/example/fase1_grupob/Fase1GrupoBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fase1_grupob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fase1GrupoBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
