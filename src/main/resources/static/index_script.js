feab1db7da65c2c5ccbcfef50ee05cca917e84d3
Juan Carlos Blanco Martínez <ju4nc4rr@gmail.com>
2024-03-10 22:31:37 +0100

    Bugs Fixed, Language Changes and new buttons

--- src/main/resources/static/index_script.js
+++ src/main/resources/static/index_script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
     $(".profilePhoto").click(function() {
-        $(".input").slideToggle(); // Alternar la visibilidad del menú al hacer clic en "QUACKSNAP"
+        $(".input").slideToggle();
     });
 });
